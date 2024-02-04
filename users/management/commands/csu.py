a90799db946b1a5831ce0af2a1827870b1a292d3
Pavel Glavan <pglavan1998@mail.ru>
2024-02-04 18:50:50 +0300

    modify csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,8 +6,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             email='admin@mail.ru',
-            first_name='Admin',
-            last_name='Admin'
+            first_name='Adminfirst',
+            last_name='Adminlast',
+            is_staff=True,
+            is_superuser=True
         )
         user.set_password('admin')
         user.save()
