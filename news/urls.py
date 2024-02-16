63036cde388aed147f7f0020d42b4dc5655bdd4e
Pavel Glavan <pglavan1998@mail.ru>
2024-02-16 06:01:12 +0300

    add news delete view

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from news.apps import NewsConfig
-from news.views import NewsCreationView, NewsListView, NewsDetailView, NewsUpdateView
+from news.views import NewsCreationView, NewsListView, NewsDetailView, NewsUpdateView, NewsDeleteView
 
 app_name = NewsConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list/', NewsListView.as_view(), name='list'),
     path('view/<str:slug>/', NewsDetailView.as_view(), name='news_detail'),
     path('update/<str:slug>/', NewsUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', NewsDeleteView.as_view(), name='delete'),
 ]
